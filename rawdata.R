c47e54a5f3d6a657aa47c39dc5b49b8ec965965c
Souhaib <bsouhaib@gmail.com>
2017-01-03 04:45:36 +0000

    transfer

--- rawdata.R
+++ rawdata.R
@@ -5,4 +5,27 @@ library(dplyr)
 library(lubridate)
 library(gdata)
 
-system("aws s3 ls  s3://code-old-ukmeters")
+#system("aws s3 ls  s3://code-old-ukmeters")
+
+
+library("aws.s3")
+Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAI7CHVBJDZPHKX72Q",
+           "AWS_SECRET_ACCESS_KEY" = "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",
+           "AWS_DEFAULT_REGION" = "ap-southeast-2",
+           "AWS_SESSION_TOKEN" = "")
+bucketlist()
+get_bucket(bucket = 'code-old-ukmeters')
+
+# Copy data set from S3
+save_object("edrp_geography_data.xlsx", bucket = "data-ukmeters", 
+                 file = "local_edrp_geography_data.xls")
+save_object("edrp_elec.csv", bucket = "data-ukmeters", 
+                 file = "local_edrp_elec.csv")
+
+# Create Rdata files and save them in S3
+
+
+
+
+# Delete data set in S3 + in instance
+
